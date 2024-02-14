807724b81a7084d166815143fdd3c1beef14684f
td2e222 <130566291+td2e222@users.noreply.github.com>
2024-02-14 02:15:16 +0000

    Update spooner.py

--- spooner.py
+++ spooner.py
@@ -1,4 +1,3 @@
-#Duplicate code example
 def calculate_area(length, width):
   area = length * width
   return area
@@ -18,9 +17,15 @@ def calculate_surface_area(length, width, height):
   surface_area = 2 * (length * width + length * height + width * height)
   return surface_area
 
-
-# Duplicate code: calculate_area and calculate_perimeter functions have similar calculations.
 def calculate_area_perimeter(length, width):
   area = length * width
   perimeter = 2 * (length + width)
   return area, perimeter
+
+
+def main():
+  area, perimeter = calculate_area_perimeter(5,10);
+  print(area, perimeter)
+
+if __name__ == '__main__':
+  main()
